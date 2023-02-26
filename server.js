99ac0391cb68f641a5c746fafa3d9a7bed5af014
Srinivas-bz <srinivas@beingzero.in>
2023-02-26 14:14:58 +0530

    adding mongo to the project

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
 app.use(express.static("frontend"))
+const mongoose = require('mongoose');
+const dbString = process.env.MONGO_CONNECTION_STRING;
+
+mongoose.set('strictQuery', true);
+mongoose.connect(dbString,(err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        app.listen(3000,()=>{
+            console.log("server up and running");
+        });
+        console.log("db Connected successfully");
+    }
+});
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/frontend/simple.html");
-});
-
-app.listen(3000,()=>{
-    console.log("server up and running");
 });
